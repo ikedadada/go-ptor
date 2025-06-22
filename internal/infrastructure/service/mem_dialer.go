ca6cb1f9b8fb88dca38b7b7442a4ff70b76814c8
ikedadada <50313310+ikedadada@users.noreply.github.com>
2025-06-22 19:17:37 +0900

    fix mem dialer waitcreated

--- internal/infrastructure/service/mem_dialer.go
+++ internal/infrastructure/service/mem_dialer.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"crypto/rand"
 	"net"
 	"time"
 
@@ -15,9 +16,15 @@ type MemDialer struct{}
 // NewMemDialer returns a CircuitDialer that performs no network I/O.
 func NewMemDialer() useSvc.CircuitDialer { return &MemDialer{} }
 
-func (MemDialer) Dial(string) (net.Conn, error)                      { return dummyConn{}, nil }
-func (MemDialer) SendCell(net.Conn, entity.Cell) error               { return nil }
-func (MemDialer) WaitCreated(net.Conn) ([]byte, error)               { return make([]byte, 32), nil }
+func (MemDialer) Dial(string) (net.Conn, error)        { return dummyConn{}, nil }
+func (MemDialer) SendCell(net.Conn, entity.Cell) error { return nil }
+func (MemDialer) WaitCreated(net.Conn) ([]byte, error) {
+	var pub [32]byte
+	if _, err := rand.Read(pub[:]); err != nil {
+		return nil, err
+	}
+	return value_object.EncodeCreatedPayload(&value_object.CreatedPayload{RelayPub: pub})
+}
 func (MemDialer) SendDestroy(net.Conn, value_object.CircuitID) error { return nil }
 
 // dummyConn implements net.Conn but does nothing.
